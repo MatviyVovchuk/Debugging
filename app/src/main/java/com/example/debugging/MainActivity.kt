b1e4218b81202a1c7e75dde68cfd6fa318ce22b3
MatviyVovchuk <doc25900@gmail.com>
2023-12-26 18:49:04 +0200

    Using logs to identify and fix the bug

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -12,12 +12,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         logging()
         division()
+        first()
+    }
+
+    fun first() {
+        second()
+        Log.v(TAG, "1")
+    }
+
+    fun second() {
+        third()
+        Log.v(TAG, "2")
+        fourth()
+    }
+
+    fun third() {
+        Log.v(TAG, "3")
+    }
+
+    fun fourth() {
+        Log.v(TAG, "4")
     }
 
     fun division() {
         val numerator = 60
         var denominator = 4
-        repeat(5) {
+        repeat(4) {
+            Log.d(TAG, "$denominator")
             Log.v(TAG, "${numerator / denominator}")
             denominator--
         }
